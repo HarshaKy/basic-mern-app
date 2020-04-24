1dcae61b956f5d95fd757898b40a69090b0dc249
HarshaKy <harshaky6573@gmail.com>
2020-04-24 20:32:31 +0530

    search user by name and ID

--- server/server.js
+++ server/server.js
@@ -36,6 +36,46 @@ app.get('/users', (req, res) => {
     })
 })
 
+app.get('/users/id/:id', (req, res) => {
+    var id = req.params.id
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send('Not Valid')
+    }
+
+    User.findById(id).then((user) => {
+        if(!user) {
+            res.status(404).send('User not found')
+        }
+        res.send({user})
+    }).catch((e) => console.log('Invalid ID'))
+})
+
+app.get('/users/name/:name', (req, res) => {
+    var name = req.params.name
+
+    User.find({name}).then((user) => {
+        if(Object.keys(user).length == 0) {
+            res.status(404).send('User not found')
+        }
+        res.send({user})
+    }).catch((e) => console.log('Invalid ID'))
+})
+
+app.get('/users/address/:address', (req, res) => {
+    var address = req.params.address
+
+    var search = {
+        addresses: {
+            hometown: address
+        }
+    }
+
+    User.find({search}).then((user) => {
+        res.send({user})
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
